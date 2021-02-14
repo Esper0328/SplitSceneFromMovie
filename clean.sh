58c1ba08d26b1885181b2135dc182a285d9b86a4
Esper0328 <yoheikato@katouyouheinoMacBook-Air.local>
2021-02-14 12:25:08 +0900

    refactored script

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rm -rf output
+rm -rf image
 rm -rf movie 
 rm ffmpegOutput
 rm sceneList.txt
--- splitSceneFromMovie.py
+++ splitSceneFromMovie.py
@@ -24,32 +24,30 @@ def getSceneChangeTime(data):
     return timeList
 
 def getSceneChange(path):
+    print(path)
     fileList = os.listdir(path)
     fileList = sorted(fileList)
     return fileList
 
-def getSceneChangeIndex(fileList):
+def getSceneIndex(fileList):
     pattern = r'([0-9]+[0-9]*).jpg'
     indexList = []
     for i,j in enumerate(fileList):
         temp = re.findall(pattern, j)
         if(temp != None):
             indexList.append(temp[0])
-    indexList = [int(k) - 1 for k in indexList]
+    indexList = [int(k) for k in indexList]
+    indexList.insert(0,0)#Added Start Index
     return indexList
 
 def splitSceneFromMovie(sceneChangeIndex, movieSceneTime):
     text = ''
-    cmd = 'ffmpeg -ss 0 -i '+ sys.argv[1] + ' -t %g' % movieSceneTime[0] + ' movie/output0.mp4'
-    subprocess.call(cmd, shell=True)    
-    text += 'file output/output0.mp4\n'
     for i, j in enumerate(sceneChangeIndex):
         delta = movieSceneTime[j + 1] - movieSceneTime[j]
-        k = i + 1
         cmd = 'ffmpeg -ss %s ' % movieSceneTime[j] + \
-            ' -i ' + sys.argv[1] + ' -t %g' % delta + ' movie/output%s.mp4' % k
+            ' -i ' + sys.argv[1] + ' -t %g' % delta + ' movie/output%s.mp4' % j
         subprocess.call(cmd, shell=True)
-        text += 'file output/output' + str(k) + '.mp4\n'
+        text += 'file output/output' + str(j) + '.mp4\n'
     return text
 
 def writeFile(fileName, text):
@@ -61,25 +59,26 @@ if __name__ == '__main__':
     if len(sys.argv) != 2:
         print('Invalid Number of argument¥nUsage: python cutSceneFromMovie.py Filename')
     else:
-        if not os.path.exists('output'):
-            subprocess.call(["mkdir", "output"])
+        if not os.path.exists('image'):
+            subprocess.call(["mkdir", "image"])
 
         if not os.path.isfile('ffmpegOutput'):
             cmd = 'ffmpeg -i ' + sys.argv[1] + ' -vf '+ \
-            '\"select=gt(scene\,0.3), scale=640:360,showinfo\" ' + '-vsync vfr output/%04d.jpg -f null - 2>ffmpegOutput'
+            '\"select=gt(scene\,0.3), scale=640:360,showinfo\" ' + '-vsync vfr image/%04d.jpg -f null - 2>ffmpegOutput'
             subprocess.call(cmd, shell=True)
             
         data = loadData('ffmpegOutput')
+        sceneTime = getSceneChangeTime(data)
+        sceneTime.insert(0, 0)#Added movieStartTime
         movieEndTime = getMovieEndTime(data)
-        movieSceneTime = getSceneChangeTime(data)
-        movieSceneTime.append(movieEndTime)
+        sceneTime.append(movieEndTime)
 
-        sceneChangeFile = getSceneChange("./output")
-        sceneChangeIndex = getSceneChangeIndex(sceneChangeFile)
+        sceneChangeFile = getSceneChange("./image")
+        sceneIndex = getSceneIndex(sceneChangeFile)
         if not os.path.exists('./movie/output0.mp4'):
             if not os.path.exists('movie'):
                 subprocess.call(["mkdir", "movie"])
-            text = splitSceneFromMovie(sceneChangeIndex, movieSceneTime)
+            text = splitSceneFromMovie(sceneIndex, sceneTime)
         else:
             text = ''
             for i, j in enumerate(sceneChangeIndex):
